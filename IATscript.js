59b9063c3010a960ed269536c1a64085b276537c
danielcheong22 <118175319+danielcheong22@users.noreply.github.com>
2022-11-15 00:54:00 +0800

    Update IATscript.js

--- IATscript.js
+++ IATscript.js
@@ -1,46 +1,45 @@
 define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
-return iatExtension({
+	return iatExtension({
 		category1 : {
-			name : 'Minorities', //Will appear in the data.
+			name : 'Black people', //Will appear in the data.
 			title : {
-				media : {word : 'Minorities'}, //Name of the category presented in the task.
+				media : {word : 'Black people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'Minorities1.png'},
-			{image : 'Minorities2.png'},
-			{image : 'Minorities3.jpg'}, 
-		    	{image : 'Minorities4.png'}, 
-    			{image : 'Minorities5.png'}, 
-    			{image : 'Minorities6.png'}
+    		    {image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Chinese', //Will appear in the data.
+			name : 'White people', //Will appear in the data.
 			title : {
-				media : {word : 'Chinese'}, //Name of the category presented in the task.
+				media : {word : 'White people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'Chinese1.png'},
-			{image : 'Chinese2.png'},
-			{image : 'Chinese3.jpg'},
-			{image : 'Chinese4.jpg'},
-		    	{image : 'Chinese5.png'},
-       			{image : 'Chinese6.jpg'}
-			], 
+    		    {image : 'yf1.jpg'}, 
+    			{image : 'yf4.jpg'}, 
+    			{image : 'yf5.jpg'}, 
+    			{image : 'ym2.jpg'}, 
+    			{image : 'ym3.jpg'}, 
+    			{image : 'ym5.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://danielcheong22.github.io/iat/images/'
+			image : 'https://baranan.github.io/minno-tasks/images/'
 		} 
 	});
 });
