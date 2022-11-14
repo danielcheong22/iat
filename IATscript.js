8baaa3aee1407a7fec78c4618fb912c01dd5470a
danielcheong22 <118175319+danielcheong22@users.noreply.github.com>
2022-11-14 19:50:21 +0800

    Update IATscript.js

--- IATscript.js
+++ IATscript.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://https://github.com/danielcheong22/iat/IATscript.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
