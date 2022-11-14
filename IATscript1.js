d0f7870fc919a58d55908b3d62408d8a93bf4165
danielcheong22 <118175319+danielcheong22@users.noreply.github.com>
2022-11-14 19:39:18 +0800

    Update IATscript1.js

--- IATscript1.js
+++ IATscript1.js
@@ -1,45 +1,109 @@
 define(['pipAPI', 'https://github.com/danielcheong22/iat/blob/9a62236cc43174e3a760f2534c3966e6c2d70f65/IATscript1.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
-	return iatExtension({
-		category1 : {
-			name : 'Minorities', //Will appear in the data.
+//Randomly choose the attribute words from a larger list of attribute words.
+  //Internal studies in Project Implicit found no difference in performance between the words.
+  var posWords = API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure', 
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
+  var negWords = API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
+  
+    return iatExtension({
+        category1 : {
+            name : 'Black people', //Will appear in the data.
+            title : {
+                media : {word : 'Black people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
+                {image: 'bm2_nc.jpg'},
+                {image: 'bm3_nc.jpg'},
+				{image: 'bf1_nc.jpg'},
+                {image: 'bf2_nc.jpg'},                 
+				{image: 'bf3_nc.jpg'}     
+    	    ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 :    {
+            name : 'White people', //Will appear in the data.
+            title : {
+                media : {word : 'White people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+				{image: 'wf1_nc.jpg'},
+            	{image: 'wf2_nc.jpg'},
+            	{image: 'wf3_nc.jpg'}
+            ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+		attribute1 :
+		{
+			name : 'Bad',
 			title : {
-				media : {word : 'Minorities'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				media : {word : 'Bad'},
+				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
-			}, 
+			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'Minorities1.jpg'}, 
-    			{image : 'Minorities2.jpg'}, 
-    			{image : 'Minorities3.jpg'}, 
-    			{image : 'Minorities4.jpg'}, 
-    			{image : 'Minorities5.jpg'}, 
-    			{image : 'Minorities6.jpg'}
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'Chinese', //Will appear in the data.
-			title : {
-				media : {word : 'Chinese'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'Chinese1.jpg'}, 
-    			{image : 'Chinese2.jpg'}, 
-    			{image : 'Chinese3.jpg'}, 
-    			{image : 'Chinese4.jpg'}, 
-    			{image : 'Chinese5.jpg'}, 
-    			{image : 'Chinese6.jpg'}			], 
+				{word: negWords[0]},
+				{word: negWords[1]},
+				{word: negWords[2]},
+				{word: negWords[3]},
+				{word: negWords[4]},
+				{word: negWords[5]},
+				{word: negWords[6]},
+				{word: negWords[7]}
+			],
 			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-
-		base_url : {//Where are your images at?
-			image : 'https://danielcheong22.github.io/iat/images/'
-		} 
-	});
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Good',
+			title : {
+				media : {word : 'Good'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: posWords[0]},
+				{word: posWords[1]},
+				{word: posWords[2]},
+				{word: posWords[3]},
+				{word: posWords[4]},
+				{word: posWords[5]},
+				{word: posWords[6]},
+				{word: posWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+        base_url : {//Where are your images at? You might want to copy the photos to your own server
+            image : 'https://baranan.github.io/minno-tasks/images/'
+        }
+    });
 });
+
